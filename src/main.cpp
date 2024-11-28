3c8599c1a5ffc8175e1cff575b5a6f6c2b686742
Michael Hemph <michael.hemph@gmail.com>
2024-11-28 09:35:25 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+// put global variables here
+
+void setup() {
+  // put your setup code here, to run once
+}
+
+void loop() {
+  // put your main code here, to run repeatedly
+}
